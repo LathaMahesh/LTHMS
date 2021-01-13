b405b3123a9d25cc551326a3eb1688943668bf70
Latha <lathakm.1993@gmail.com>
2021-01-13 13:34:08 +0530

    c4

--- LTMS/src/apple.py
+++ LTMS/src/apple.py
@@ -3,3 +3,8 @@
 feature 6: support single sign on with apple servers
 feature 7: support ipad resolutions#apple_feture_1
 #adding Feature 2
+<<<<<<< HEAD
+=======
+#adding Feature 3
+#adding Feature 4
+>>>>>>> 50b0441... c4
